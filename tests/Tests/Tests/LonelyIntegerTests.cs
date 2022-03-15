d38596b7651fbfca456af1d483e1f9ac0f5b387a
Aislan Michel Moreira Freitas <aislan.michel92@gmail.com>
2022-03-14 23:37:50 -0300

    finish lonely integer

---
+++ tests/Tests/Tests/LonelyIntegerTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Tests
+{
+    public class LonelyIntegerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var input = new List<int>(3) { 1, 1, 2 };
+
+            var result = Result.Lonelyinteger(input);
+
+            Assert.Equal(2, result);
+        }
+    }
+}
