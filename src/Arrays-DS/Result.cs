326e77fa88850d9948c5956175f4647b254f6aef
Aislan Michel Moreira Freitas <aislan.michel92@gmail.com>
2022-04-05 12:10:38 -0300

    finish ReverseArray

---
+++ src/Arrays-DS/Result.cs
@@ -0,0 +1,32 @@
+﻿namespace Arrays_DS
+{
+    public static class Result
+    {
+        public static List<int> ReverseArray(List<int> arr)
+        {
+            if(arr == null)
+            {
+                throw new ArgumentNullException(nameof(arr));
+            }
+
+            if (!arr.Any())
+            {
+                throw new ArgumentOutOfRangeException(nameof(arr));
+            }
+
+            var reversedArray = new List<int>() { Capacity = arr.Length() };
+            
+            for (int i = arr.Length(); i >= 0; i--)
+            {
+                reversedArray.Add(arr[i]);
+            }
+
+            return reversedArray;
+        }
+
+        private static int Length(this List<int> list)
+        {
+            return list.Count - 1;
+        }
+    }
+}
